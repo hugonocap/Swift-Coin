cea1cea5b603da539cbfdec4146373b5a313401e
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-04 00:02:26 +0200

    project setup

--- Swift Coin/App/Swift_CoinApp.swift
+++ Swift Coin/App/Swift_CoinApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Swift_CoinApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ Swift Coin/Core/Home/Views/HomeView.swift
@@ -0,0 +1,24 @@
+//
+//  HomeView.swift
+//  Swift Coin
+//
+//  Created by Hugo on 3.11.22.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        NavigationView {
+            ScrollView(.vertical, showsIndicators: false) {
+                
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
