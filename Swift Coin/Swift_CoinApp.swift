3db006ad2f242679f7e8ed2a6e518e3fb668354c
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-03 23:52:54 +0200

    Initial Commit

---
+++ Swift Coin/Swift_CoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_CoinApp.swift
+//  Swift Coin
+//
+//  Created by Hugo on 3.11.22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_CoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
