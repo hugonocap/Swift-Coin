9715a60ea6f4df5a95dad5b8e83674f4247a8405
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-04 10:55:10 +0200

    coin raw value

--- Swift Coin/Core/Home/Views/CoinRawView.swift
+++ Swift Coin/Core/Home/Views/CoinRawView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct CoinRawView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            // MARK: Market cap rank
+             Text("1")
+                .font(.caption)
+                .foregroundColor(.gray )
+            
+            // MARK: Image
+            Image(systemName: "bitcoinsign.circle.fill")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 32, height: 32)
+                .foregroundColor(.orange)
+            
+            // MARK: Coin name info
+            VStack(alignment: .leading, spacing: 4) {
+                Text("Bitcoin")
+                    .padding(.leading, 4)
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                Text("BTC")
+                    .padding(.leading, 6 )
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .padding(.leading, 2)
+            
+            Spacer()
+            
+            // MARK: Coin price info
+            VStack(alignment: .trailing, spacing: 4) {
+                Text("$20.330.00")
+                    .font(.subheadline)
+                    .fontWeight(.bold)
+                Text("-5.60%")
+                    .font(.caption)
+                    .foregroundColor(.red)
+            }
+            
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 4)
     }
 }
 
