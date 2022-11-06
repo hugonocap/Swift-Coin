9efbe2224d584b7cc456c297528de50db25de304
Hugo Montana <benzlbenny2k@gmail.com>
2022-11-06 17:14:33 +0200

    price formatter

---
+++ Swift Coin/Models/Double.swift
@@ -0,0 +1,25 @@
+//
+//  Double.swift
+//  Swift Coin
+//
+//  Created by Hugo on 6.11.22.
+//
+
+import Foundation
+
+extension Double {
+    
+    private var currencyFormatter: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    func toCurrency() -> String {
+        return currencyFormatter.string(for: self) ?? "$0.00"
+    }
+    
+}
